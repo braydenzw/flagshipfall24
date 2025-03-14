1df4b664bd5afa2678e720b7181586ec1264449e
Phoenix-Jarosz <jarosz@wustl.edu>
2025-03-14 15:38:35 -0500

    Phoenix Cut Game

--- Assets/Scripts/ScrollingBars1.cs
+++ Assets/Scripts/ScrollingBars1.cs
@@ -1,38 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Security.Cryptography;
+
+
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 using System;
+using System.Diagnostics;
+
 //using static System.Net.Mime.MediaTypeNames;
 
-/*
-* This is an actual Unity script that should allow the player to interact with this station
-*  The actual implementation of this is up to whoever is scripting this...
-*  
-*  Requirements:
-*   - User can interact with the station somehow
-*   - When interacting, user can:
-*       - Place a pizza down in an unoccupied slot
-*       - Pick up a pizza from an occupied slot
-*       - Begin some mini-game related to the station
-*           - Based on the performance in mini-game, Pizza object is updated
-*       - Send out/submit a pizza object
-*/
-
-
-
-public class CutStation : MonoBehaviour
+public class ScrollingBars : MonoBehaviour
 {
-    const int pizzaCutSlots = 10; // TODO: define number of slots here, can be whatever you think is appropriate
-    private Pizza[] pizzas; // ordered list of pizza data objects
-    public DayManager dayManager; // needed for order submissions
-
-
-    public GameObject barSprite; 
-    public float barSpeed = 20f; 
-    public float screenPadding = 0.5f; 
+    public GameObject barSprite; // Reference to the specific sprite for the bar
+    public float barSpeed = 20f; // Speed of bar movement
+    public float screenPadding = 0.5f; // Padding for the bar position
 
     private float screenWidth;
     public int sliceCount = 4;
@@ -43,23 +23,27 @@ public class CutStation : MonoBehaviour
 
     SpriteRenderer spriteSettings;
 
-
     void Start()
     {
-        
-        pizzas = new Pizza[pizzaCutSlots];
-        
-
+        // Get screen width in world units
         screenWidth = Camera.main.orthographicSize * Camera.main.aspect * 2;
         scoreDisplay.enabled = true;
         scoreDisplay.text = "Score: " + score;
-        spriteSettings = GetComponent<SpriteRenderer>(); //I still can't get text to work, but this would display the score
-
+        spriteSettings = GetComponent<SpriteRenderer>();
+        //if (barSprite == null)
+        //{
+        //    Debug.LogError("Bar sprite is not assigned!");
+        //}
     }
 
-    
+    int returnScore (int cutScore)
+    {
+        return cutScore;
+    }
+
     void Update()
     {
+        
         if (barSprite != null)
         {
             // Move the bar to the right
@@ -74,23 +58,25 @@ public class CutStation : MonoBehaviour
                 barSpeed = barSpeed + 12f;
                 sliceCount--;
             }
-            
+            if (sliceCount == 1) {
+                returnScore(score);
+            }
         }
-        if (Input.GetKeyDown(KeyCode.Space)) //Click space when moving bar is over square. You have 4 tries
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-
+            
             if (clickAttempts > 0)
             {
                 float distance = Math.Abs(barSprite.transform.position.x - (screenWidth / 2f)) / (screenWidth / 2f);
-                score += ((int)(distance * 30)); //I made the scoring generous :)
+                score += ((int) (distance * 30));
                 if (score > 100)
                 {
                     score = 100;
                 }
-
+                
             }
             clickAttempts--;
-            if (clickAttempts == 3)
+            if(clickAttempts == 3)
             {
                 spriteSettings.color = new Color(1, 0, 0, 1);
             }
@@ -109,13 +95,8 @@ public class CutStation : MonoBehaviour
         }
         scoreDisplay.text = "Score: " + score;
     }
+
+    
 }
-    // TODO: something handling user interaction
-    // TODO: something handling placement of pizza object (put down and pick up)
-    // TODO: something handling actual mini-game
-    // mini-game should reference some index in pizzas array to update Pizza class
-    // TODO: something handling submission of pizza object (should reference DayManager function)
-
-
 
 
